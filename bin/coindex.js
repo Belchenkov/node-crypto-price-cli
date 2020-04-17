476226e9f67255f1e46cbe1cf791572ced4e5ca1
Aleksey Belchenkov <u608110@gmail.com>
2020-04-17 19:26:00 +0300

    Get coins and currency from console

--- bin/coindex.js
+++ bin/coindex.js
@@ -5,5 +5,6 @@ const pkg = require('../package.json');
 program
     .version(pkg.version)
     .command('key', 'Manage API Key -- https://nomics.com')
+    .command('check', 'Check Coin Price Info')
     .parse(process.argv);
 
